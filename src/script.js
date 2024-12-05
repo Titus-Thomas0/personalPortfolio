ec762b93ffa3de473c429b0c6a1ef23d29fd9052
Titus Thomas <tttitus99@gmail.com>
2024-12-05 12:55:52 +0530

    all the items was ready for nav bar now moving to styling it

--- src/script.js
+++ src/script.js
@@ -1,6 +1,10 @@
 //Script to apply for Titus Thomas Personal Portfolio
 
-//==========MenuIcon Toogle script==========
-function menuToggle(x) {
-    x.classList.toggle('open');
+//Declare your gobal variables here=====>
+const menuIcon = document.getElementById('menuIcon');//==========MenuIcon Toogle script==========
+
+//Code here=====>
+//==========Function for MenuIcon to Toogle the class to open==========
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('open');
 }
