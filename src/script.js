a3e407fd196f5d8843c81dbbd0767ac0de828d37
Titus Thomas <tttitus99@gmail.com>
2024-12-09 21:13:41 +0530

    Header is changed after review and home screen has been added.

--- src/script.js
+++ src/script.js
@@ -1,15 +1,13 @@
 //Script to apply for Titus Thomas Personal Portfolio
 
-//Declare your gobal variables here=====>
-const menuIcon = document.getElementById('menuIcon');//==========MenuIcon Toogle script==========
+//Declare your global variables here=====>
+const menuIcon = document.getElementById('menuIcon');//==========MenuIcon Toggle script==========
 const navItem = document.getElementById('navItem');
-const logoImg = document.getElementById('logoImg');
-const logoText = document.getElementById('logoText');
 
 navItem.style.maxHeight = '0px';
 
 //Code here=====>
-//==========Function for MenuIcon to Toogle the class to open==========
+//==========Function for MenuIcon to Toggle the class to open==========
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('open');
     if (navItem.style.maxHeight == '0px') {
@@ -20,26 +18,16 @@ menuIcon.onclick = () => {
     }
 }
 
-logoImg.addEventListener('mouseover', () => {
-    logoImg.style.transform = 'rotate(30deg)';
-    logoImg.style.transition = 'all 0.05s';
-    logoText.style.color = '#6A0C0B';
-});
+let greetIndex = 0;
+let greetText = '<Hello, World!>';
+const speed = 150;
 
-logoImg.addEventListener('mouseout', () => {
-    logoImg.style.transform = 'rotate(0deg)';
-    logoImg.style.transition = 'all 0.05s';
-    logoText.style.color = '';
-});
+function greetings() {
+    if (greetIndex < greetText.length) {
+        document.getElementById('greetings').innerHTML += greetText.charAt(greetIndex);
+        greetIndex++;
+        setTimeout(greetings, speed);
+    }
+}
 
-logoText.addEventListener('mouseover', () => {
-    logoImg.style.transform = 'rotate(30deg)';
-    logoImg.style.transition = 'all 0.05s';
-    logoText.style.color = '#6A0C0B';
-});
-
-logoText.addEventListener('mouseout', () => {
-    logoImg.style.transform = 'rotate(0deg)';
-    logoImg.style.transition = 'all 0.05s';
-    logoText.style.color = '';
-});
+greetings();
