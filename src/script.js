c121264774af4e236abfc38b5397358bca79d7c5
Titus Thomas <tttitus99@gmail.com>
2025-01-03 22:47:33 +0530

    Missing    src/assets/images/Logo-1.png Missing    src/assets/images/Logo.png Modified   src/index.html Modified   src/script.js Modified   src/style.css Added      .vscode/settings.json Added      src/assets/images/BG.jpg Added      src/assets/images/Titus Photo.jpg Added      src/assets/images/logo1.jpg

--- src/script.js
+++ src/script.js
@@ -3,11 +3,11 @@
 //Declare your global variables here=====>
 const menuIcon = document.getElementById('menuIcon');//==========MenuIcon Toggle script==========
 const navItem = document.getElementById('navItem');
-
-navItem.style.maxHeight = '0px';
+const navList = document.getElementsByClassName('navList');
 
 //Code here=====>
-//==========Function for MenuIcon to Toggle the class to open==========
+//==========Function for MenuIcon to Toggle the id to open==========
+navItem.style.maxHeight = '0px';
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('open');
     if (navItem.style.maxHeight == '0px') {
@@ -18,8 +18,12 @@ menuIcon.onclick = () => {
     }
 }
 
+//==========Function for showing active item in Navbar==========
+
+
+//==========Function for creating welcome message to appear==========
 let greetIndex = 0;
-let greetText = '<Hello, World!>';
+let greetText = '<HELLO, WORLD!>';
 const speed = 150;
 
 function greetings() {
